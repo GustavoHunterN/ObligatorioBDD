f5f56e1cf429698eb4ec5b94ff25bbd39f709a16
Gustavo Hunter <gustavohunter@MacBook-Pro-de-Gustavo.local>
2025-11-07 17:30:40 -0300

    Simetria con Clases

--- DB/Tables.sql
+++ DB/Tables.sql
@@ -1,86 +1,95 @@
 USE OBL;
-
+-- LOGIN ----------------------------------------------------
 CREATE TABLE login(
     correo VARCHAR(255) NOT NULL UNIQUE,
     contraseña VARCHAR (64) NOT NULL
 );
 
-
-
+-- PARTICIPANTE ---------------------------------------------
 CREATE TABLE participante (
-    ci VARCHAR(8) NOT NULL UNIQUE PRIMARY KEY,
+    ci VARCHAR(8) NOT NULL PRIMARY KEY,
     nombre VARCHAR(15) NOT NULL,
     apellido VARCHAR(15) NOT NULL,
     correo VARCHAR(30) NOT NULL UNIQUE,
     FOREIGN KEY (correo) REFERENCES login(correo)
 );
 
-
+-- SANCION ---------------------------------------------------
 CREATE TABLE sancion_participante(
-    ci VARCHAR(8) NOT NULL UNIQUE,
+    id_sancion INT PRIMARY KEY AUTO_INCREMENT,
+    ci VARCHAR(8) NOT NULL,
     fecha_inicio DATETIME NOT NULL,
     fecha_final DATETIME,
     FOREIGN KEY (ci) REFERENCES participante(ci)
 );
 
+-- FACULTAD --------------------------------------------------
 CREATE TABLE facultad(
-    id_facultad VARCHAR(6) NOT NULL UNIQUE,
+    id_facultad VARCHAR(6) NOT NULL PRIMARY KEY,
     nombre VARCHAR(15)
 );
 
-
-
+-- PROGRAMA --------------------------------------------------
 CREATE TABLE programa_academico(
-    nombre_programa VARCHAR(20) NOT NULL UNIQUE PRIMARY KEY,
-    id_facultad VARCHAR(6) NOT NULL UNIQUE,
+    nombre_programa VARCHAR(20) NOT NULL PRIMARY KEY,
+    id_facultad VARCHAR(6) NOT NULL,
     tipo VARCHAR(8),
     FOREIGN KEY (id_facultad) REFERENCES facultad(id_facultad)
-                               );
+);
 
-
+-- PARTICIPANTE_PROGRAMA -------------------------------------
 CREATE TABLE participante_programa(
-    id_part_prog VARCHAR(15) PRIMARY KEY,
+    id_part_prog INT PRIMARY KEY AUTO_INCREMENT,
     rol VARCHAR(20) NOT NULL,
-    nombre_programa VARCHAR(50),
-    ci VARCHAR(8) NOT NULL UNIQUE,
+    nombre_programa VARCHAR(20),
+    ci VARCHAR(8) NOT NULL,
     FOREIGN KEY (nombre_programa) REFERENCES programa_academico(nombre_programa),
     FOREIGN KEY (ci) REFERENCES participante(ci)
 );
 
-
+-- EDIFICIO ---------------------------------------------------
 CREATE TABLE edificio(
+    nombre_edificio VARCHAR(15) NOT NULL PRIMARY KEY,
     direccion VARCHAR(30),
-    departamento VARCHAR(10),
-    nombre_edif VARCHAR(15) NOT NULL UNIQUE
+    departamento VARCHAR(10)
 );
-
-CREATE TABLE turnos(
-    id_turno VARCHAR(20) PRIMARY KEY,
-    hora_inicio TIME,
-    hora_final TIME
-);
-
+-- SALA -------------------------------------------------------
 CREATE TABLE sala(
-    nombre_sala VARCHAR(5) NOT NULL UNIQUE,
+    nombre_sala VARCHAR(5) NOT NULL PRIMARY KEY,
     capacidad INT NOT NULL,
     tipo_sala VARCHAR(8),
-    edificio  VARCHAR(15) NOT NULL,
-    FOREIGN KEY (edificio) REFERENCES edificio(nombre_edif)
+    edificio VARCHAR(15) NOT NULL,
+    FOREIGN KEY (edificio) REFERENCES edificio(nombre_edificio)
 );
+
+-- TURNOS -----------------------------------------------------
+CREATE TABLE turnos(
+    id_turno INT PRIMARY KEY AUTO_INCREMENT,
+    hora_inicio TIME,
+    hora_final TIME,
+    dia DATE,
+    nombre_sala VARCHAR(5),
+    FOREIGN KEY (nombre_sala) REFERENCES sala(nombre_sala)
+);
+
+
+-- RESERVA ----------------------------------------------------
 CREATE TABLE reserva(
-    id_reserva VARCHAR(20) PRIMARY KEY ,
+    id_reserva INT PRIMARY KEY AUTO_INCREMENT,
     fecha DATETIME,
     estado VARCHAR(14) NOT NULL,
     id_turno INT NOT NULL,
     nombre_edificio VARCHAR(15) NOT NULL,
-    sala  VARCHAR(5) NOT NULL,
-    FOREIGN KEY (nombre_edificio) REFERENCES edificio(nombre_edif),
+    sala VARCHAR(5) NOT NULL,
+    FOREIGN KEY (nombre_edificio) REFERENCES edificio(nombre_edificio),
     FOREIGN KEY (id_turno) REFERENCES turnos(id_turno),
     FOREIGN KEY (sala) REFERENCES sala(nombre_sala)
 );
 
+-- RESERVA_PARTICIPANTE ---------------------------------------
 CREATE TABLE reserva_participante (
-    ci VARCHAR(8) NOT NULL UNIQUE PRIMARY KEY,
+    id_reserva_part INT PRIMARY KEY AUTO_INCREMENT,
+    ci VARCHAR(8) NOT NULL,
     id_reserva INT NOT NULL,
     fecha_solicitud DATETIME,
     asistencia TINYINT(1),
@@ -88,5 +97,3 @@ CREATE TABLE reserva_participante (
     FOREIGN KEY (ci) REFERENCES participante(ci)
 );
 
-DROP DATABASE OBL;
-CREATE DATABASE OBL;
